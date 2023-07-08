7256bec873a5ab925b49cceb5e5869e394190a1c
AyeshiJ <ayeshi.j12@gmail.com>
2023-07-08 09:47:40 +0530

    UserContext Updated

--- client/src/UserContext.jsx
+++ client/src/UserContext.jsx
@@ -2,22 +2,22 @@ import { createContext } from "react";
 
 export const UserContext = createContext({});
 
-
-
 export function UserContextProvider({children}) {
-    const [user,setUser] = useState(null);
-    const [ready,setReady] = useState(false);
-    useEffect(() => {
-      if (!user) {
-        axios.get('/profile').then(({data}) => {
-          setUser(data);
-          setReady(true);
-        });
-      }
-    }, []);
+    // const [user,setUser] = useState(null);
+    // const [ready,setReady] = useState(false);
+    // useEffect(() => {
+    //   if (!user) {
+    //     axios.get('/profile').then(({data}) => {
+    //       setUser(data);
+    //       setReady(true);
+    //     });
+    //   }
+    // }, []);
     return (
-      <UserContext.Provider value={{user,setUser,ready}}>
-        {children}
-      </UserContext.Provider>
+    //   <UserContext.Provider value={{user,setUser,ready}}>
+    <div> 
+    {children}
+    </div>  
+    //   </UserContext.Provider>
     );
   }
