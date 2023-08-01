4b3dc443604d258b3940ff9c87cc96057404f71a
AyeshiJ <ayeshi.j12@gmail.com>
2023-08-01 21:43:52 +0530

    Parks Page created

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("./models/User.js");
 const cookieParser = require("cookie-parser");
+const imageDownloader = require('image-downloader');
 const app = express();
 
 const bcryptSalt = bcrypt.genSaltSync(10);
@@ -88,4 +89,16 @@ app.post('/logout', (req, res) => {
   res.cookie('token', '').json(true);
 });
 
-app.listen(4000);
+console.log({__dirname})
+app.post('/upload-by-link', async (req,res) => {
+  const {link} = req.body;
+  const newName =  Date.now() + '.jpg';
+  await imageDownloader.image({
+    url: link,
+    dest: __dirname + '/tmp/' +newName,
+  });
+  // const url = await uploadToS3('/tmp/' +newName, newName, mime.lookup('/tmp/' +newName));
+  res.json(__dirname + '/tmp/' +newName);
+});
+
+app.listen(4000); 
